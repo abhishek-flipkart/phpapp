879b83d32b9410ed483a6e8ee2fd4f1d14fed34d
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-27 23:37:25 +0530

    Bookmark

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>A bookmark apoplication</title>
     </head>
     <body>
-        <a href="tool.js">Bookmark</a>
+        <a href="javascript:void(function(){ alert('OK');})">Bookmark</a>
     </body>
 </html>
