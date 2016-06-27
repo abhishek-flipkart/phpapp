65a2d501003d1eec76df848563d2b0f20e08277c
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 00:05:06 +0530

    Demo

--- index.php
+++ index.php
@@ -4,18 +4,8 @@
         <title>A bookmark apoplication</title>
     </head>
     <body>
-        <a href="javascript:void((function(){ var scriptId=document.getElementById('bagitscript'); console.log(scriptId); })()) ">Bookmark</a>
+        <a href="javascript:void((function(doc){var tool=document.getElementById('toolID'),toolScript=document.createElement('SCRIPT'),toolScript.type='text/javascript',toolScript.id='toolID',toolScript.src='https://productcostnotifier.herokuapp.com/tool.js';doc.getElementsByTagName('body')[0].appendChild(toolScript);
+})()document);">Bookmark</a>
         <script>
-            
-        </script>
     </body>
 </html>
-
-<!--
-javascript:(function(){
-
-var scriptId=document.getElementById('bagitscript');
-null!=scriptId&&scriptId.parentNode.removeChild(scriptId),bagIT=document.createElement('SCRIPT'),bagIT.type='text/javascript',bagIT.id='bagitscript',bagIT.src='http://indigobag.com/indigobag/lib/socialmarklet.js',document.getElementsByTagName('body')[0].appendChild(bagIT);
-
-
-})();-->
