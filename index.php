ac4a9d88e8f6b989747b68bd0943db8122adfd96
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-27 23:52:16 +0530

    check

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
         <title>A bookmark apoplication</title>
     </head>
     <body>
-        <a href="javascript:void((function(){ alert('OK');})()) ">Bookmark</a>
+        <a href="javascript:void((function(){ var scriptId=document.getElementById('bagitscript'); console.log(scriptId); })()) ">Bookmark</a>
+        <script>
+            
+        </script>
     </body>
 </html>
+
+<!--
+javascript:(function(){
+
+var scriptId=document.getElementById('bagitscript');
+null!=scriptId&&scriptId.parentNode.removeChild(scriptId),bagIT=document.createElement('SCRIPT'),bagIT.type='text/javascript',bagIT.id='bagitscript',bagIT.src='http://indigobag.com/indigobag/lib/socialmarklet.js',document.getElementsByTagName('body')[0].appendChild(bagIT);
+
+
+})();-->
