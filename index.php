19d9f56ed0933f2d1abc66ef777584c7eb4ad089
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 00:11:48 +0530

    Was running

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     </head>
     <body>
         <a href="javascript:void((function(doc){var tool=document.getElementById('toolID'),toolScript=document.createElement('SCRIPT'),toolScript.type='text/javascript',toolScript.id='toolID',toolScript.src='https://productcostnotifier.herokuapp.com/tool.js';doc.getElementsByTagName('body')[0].appendChild(toolScript);
-})()document);">Bookmark</a>
-        <script>
+})(document));">Bookmark</a>
     </body>
 </html>
+
+
