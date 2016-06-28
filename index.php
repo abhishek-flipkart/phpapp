9941df29ae36bbf4c7c6c5dfe4ccffc227ebcc7a
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 07:49:19 +0530

    Completed Bookmarks

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>A bookmark apoplication</title>
     </head>
     <body>
-        <a href="javascript:void((function(d){var spyToolID = d.getElementById('spyer'),spyTool = d.createElement('SCRIPT'),spyTool.type = 'text/javascript',spyTool.id = 'spyer',spyTool.src = 'https://productcostnotifier.herokuapp.com/tool.js';d.getElementsByTagName('body')[0].appendChild(spyTool);})(document); )">Bookmark</a>
+        <a href="javascript:void((function(d){var spyToolID = d.getElementById('spyer'); var spyTool = d.createElement('SCRIPT');spyTool.type = 'text/javascript';spyTool.id = 'spyer';spyTool.src = 'https://productcostnotifier.herokuapp.com/tool.js';d.getElementsByTagName('body')[0].appendChild(spyTool);})(document); )">Bookmark</a>
     </body>
 </html>
