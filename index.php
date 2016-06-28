6d7234d924fb80bc29e3fe54f04af8748249691f
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 08:52:35 +0530

    Successfully added spy script

--- index.php
+++ index.php
@@ -4,15 +4,6 @@
         <title>A bookmark apoplication</title>
     </head>
     <body>
-        <a href='javascript:void(function(e){var t=e.getElementById("spyer");null!=t&&t.parentNode.removeChild(t);var o=e.createElement("SCRIPT");o.type="text/javascript",o.id="spyer",o.src="https://productcostnotifier.herokuapp.com/tool.js",e.getElementsByTagName("body")[0].appendChild(o)}(document))'>Bookmark</a>
+        <a href='javascript:void(function(e){var t=e.getElementById("spyer");null!=t&&t.parentNode.removeChild(t);var o=e.createElement("SCRIPT");o.type="text/javascript",o.id="spyer",o.src="https://productcostnotifier.herokuapp.com/tool.js",e.getElementsByTagName("body")[0].appendChild(o)}(document))'>SpyIT</a>
     </body>
 </html>
-<!--
-(function(d){
-    var spyerID = d.getElementById('spyer'); 
-    var spyer = d.createElement('SCRIPT');
-    spyer.type ='text/javascript';
-    spyer.id = 'spyer';
-    spyer.src='https://productcostnotifier.herokuapp.com/tool.js';
-    d.getElementsByTagName('body')[0].appendChild(spyer);
-})(document)-->
