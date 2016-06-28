2b0721ccc48d801ab3516640a571cc5953b8576c
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 17:56:26 +0530

    Moving with cursor

--- tool.js
+++ tool.js
@@ -8,7 +8,7 @@
     d.getElementsByTagName('body')[0].appendChild(cursor);
     d.addEventListener('mousemove',function(e){
         cursor.style.top = (e.clientY - 20) + 'px';
-        cursor.style.left = (e.clientY - 20) + 'px';
+        cursor.style.left = (e.clientX - 20) + 'px';
         console.log("Moving");
     })
 })(document);
