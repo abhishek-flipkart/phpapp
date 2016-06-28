c913be748b99eb54fc2823ed3291d1c50c23aae4
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 08:09:59 +0530

    Minified

--- index.php
+++ index.php
@@ -4,14 +4,18 @@
         <title>A bookmark apoplication</title>
     </head>
     <body>
-        <a href="javascript:void((function(d){var spyerID = d.getElementById('spyer');console.log(spyerID);})(document))">Bookmark</a>
+        <a href='javascript:void(!function(e){var t=(e.getElementById("spyer"),e.createElement("SCRIPT"));t.type="text/javascript",t.id="spyer",t.src="https://productcostnotifier.herokuapp.com/tool.js",e.getElementsByName("body")[0].appendChild(t)}(document);)'>Bookmark</a>
+<!--
+        <script>
+            (function(d){
+            var spyerID = d.getElementById('spyer');
+            var spyer = d.createElement('SCRIPT');
+            spyer.type ='text/javascript';
+            spyer.id = 'spyer';
+            spyer.src='https://productcostnotifier.herokuapp.com/tool.js';
+            d.getElementsByName('body')[0].appendChild(spyer);
+            })(document);
+        </script>
+-->
     </body>
 </html>
-
-<!--
-javascript:(function(){var scriptId=document.getElementById('bagitscript');null!=scriptId&&scriptId.parentNode.removeChild(scriptId),bagIT=document.createElement('SCRIPT'),bagIT.type='text/javascript',bagIT.id='bagitscript',bagIT.src='http://indigobag.com/indigobag/lib/socialmarklet.js',document.getElementsByTagName('body')[0].appendChild(bagIT);})();
-
-
-javascript:void((function(){var e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8'),e.setAttribute('src','http://stuffdot.com/components/com_stuffdotbin/js/socialmarklet.js'),document.body.appendChild(e)})(document));
-
--->
