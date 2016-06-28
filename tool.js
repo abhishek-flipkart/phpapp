58a1ae8c617864190c203877468277e3803b1645
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 19:47:15 +0530

    Moving images around mouse pointer

--- tool.js
+++ tool.js
@@ -4,17 +4,17 @@
     var imageCount = images.length;
     
     d.addEventListener('mousemove',function(e){
-        var top = (e.clientY - 20) + 'px';
-        var left = (e.clientX - 20) + 'px';
+        var top = (e.clientY - 20);
+        var left = (e.clientX - 20);
         moveImages(left,top)
     })
     
     function moveImages(xpos,ypos) {
         for (var i = 0 ;i < imageCount;i++) {
             images[i].style.position = 'fixed';
-            images[i].style.top = ypos+(i*3);
+            images[i].style.top = (ypos+(i*3)) + 'px';
             images[i].style.zIndex = 9999999999999;
-            images[i].style.left = xpos+(i*3);
+            images[i].style.left = (xpos+(i*3)) + 'px';
         }
     }
 })(document);
