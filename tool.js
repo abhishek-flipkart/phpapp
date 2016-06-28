fa104e2eefdf544809a4e4ced9cf5de6b17e3b63
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 17:42:38 +0530

    Moving with cursor

--- tool.js
+++ tool.js
@@ -1,5 +1,10 @@
 (function(d){
     'use strict';
+    var cursor = d.createElement('div');
+    cursor.style.height = "20px";
+    cursor.style.width = "20px";
+    cursor.style.backgroundColor = "#000";
+    cursor.style.position = 'absolute';
     d.addEventListener('mousemove',function(){
         console.log("Moving");
     })
