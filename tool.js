4b7d02f13e0e8d40f6125917674d51d8a6a1f964
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 22:18:32 +0530

    Adding Button

--- tool.js
+++ tool.js
@@ -1,20 +1,5 @@
 (function(d){
     'use strict';
-    var images = d.getElementsByTagName('img');
-    var imageCount = images.length;
-    
-    d.addEventListener('mousemove',function(e){
-        var top = (e.clientY - 20);
-        var left = (e.clientX - 20);
-        moveImages(left,top)
-    })
-    
-    function moveImages(xpos,ypos) {
-        for (var i = 0 ;i < imageCount;i++) {
-            images[i].style.position = 'absolute';
-            images[i].style.top = ypos + 'px';
-            images[i].style.zIndex = 100000+i;
-            images[i].style.left = (xpos+(i*10)) + 'px';
-        }
-    }
+    var products = d.getElementsByClassName('product-unit');
+    console.log(products.length);
 })(document);
