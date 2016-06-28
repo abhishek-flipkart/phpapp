081c07d46bee1cfd3091bd985437a0a5261bd67f
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 08:16:27 +0530

    Minified

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <a href="javascript:void((function(d){ var spyerID = d.getElementById('spyer'); var spyer = d.createElement('SCRIPT');spyer.type ='text/javascript';spyer.id = 'spyer';spyer.src='https://productcostnotifier.herokuapp.com/tool.js';d.getElementsByName('body')[0].appendChild(spyer);
-            })(document))">Bookmark</a>
+            })(document);)">Bookmark</a>
 <!--
         <script>
             (function(d){ var spyerID = d.getElementById('spyer'); var spyer = d.createElement('SCRIPT');spyer.type ='text/javascript';spyer.id = 'spyer';spyer.src='https://productcostnotifier.herokuapp.com/tool.js';d.getElementsByName('body')[0].appendChild(spyer);
