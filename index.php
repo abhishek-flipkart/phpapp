815167151b8cf235f293020e6a057a62a5ef86ee
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 08:31:21 +0530

    Minified

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
         <title>A bookmark apoplication</title>
     </head>
     <body>
-        <a href="javascript:void(function(d){var spyerID = d.getElementById('spyer'); var spyer = d.createElement('SCRIPT');spyer.type ='text/javascript';spyer.id = 'spyer';spyer.src='https://productcostnotifier.herokuapp.com/tool.js';console.log(d);}(document))">Bookmark</a>
+        <a href="javascript:void(function(d){var spyerID = d.getElementById('spyer'); var spyer = d.createElement('SCRIPT');spyer.type ='text/javascript';spyer.id = 'spyer';spyer.src='https://productcostnotifier.herokuapp.com/tool.js';console.log(document.getElementsByTagName('body'));}(document))">Bookmark</a>
     </body>
 </html>
+
+<!--javascript:(function(){var scriptId=document.getElementById('bagitscript');null!=scriptId&&scriptId.parentNode.removeChild(scriptId),bagIT=document.createElement('SCRIPT'),bagIT.type='text/javascript',bagIT.id='bagitscript',bagIT.src='http://indigobag.com/indigobag/lib/socialmarklet.js',document.getElementsByTagName('body')[0].appendChild(bagIT);})();-->
