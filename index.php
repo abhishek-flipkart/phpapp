5c0cf2f11a78c5af58e56f3a59ee802f9628537e
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 08:21:52 +0530

    Minified

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
         <title>A bookmark apoplication</title>
     </head>
     <body>
-        <a href="javascript:void((function(d){ var spyerID = d.getElementById('spyer'); var spyer = d.createElement('SCRIPT');
-            })(document);)">Bookmark</a>
+        <a href="javascript:void((function(d){alert('Hi)})(document);)">Bookmark</a>
 <!--
         <script>
             (function(d){ var spyerID = d.getElementById('spyer'); var spyer = d.createElement('SCRIPT');spyer.type ='text/javascript';spyer.id = 'spyer';spyer.src='https://productcostnotifier.herokuapp.com/tool.js';d.getElementsByName('body')[0].appendChild(spyer);
