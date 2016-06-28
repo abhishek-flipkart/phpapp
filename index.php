2ed3e65873e850b80531c98bb74cfae072aac08b
Abhishek Panda <abhishek.panda@flipkart.com>
2016-06-28 07:54:23 +0530

    Testing

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
         <title>A bookmark apoplication</title>
     </head>
     <body>
-        <a href="javascript:void((function(d){var spyToolID = d.getElementById('spyer'); var spyTool = d.createElement('SCRIPT');spyTool.type = 'text/javascript';spyTool.id = 'spyer';spyTool.src = 'https://productcostnotifier.herokuapp.com/tool.js';d.getElementsByTagName('body')[0].appendChild(spyTool);})(document); )">Bookmark</a>
+        <a href="javascript:void((function(d){console.log(d)})(document))">Bookmark</a>
     </body>
 </html>
+
+<!--
+javascript:(function(){var scriptId=document.getElementById('bagitscript');null!=scriptId&&scriptId.parentNode.removeChild(scriptId),bagIT=document.createElement('SCRIPT'),bagIT.type='text/javascript',bagIT.id='bagitscript',bagIT.src='http://indigobag.com/indigobag/lib/socialmarklet.js',document.getElementsByTagName('body')[0].appendChild(bagIT);})();
+
+
+javascript:void((function(){var e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8'),e.setAttribute('src','http://stuffdot.com/components/com_stuffdotbin/js/socialmarklet.js'),document.body.appendChild(e)})(document));
+
+-->
